7ff65140ce7fa30037bd095db4142672bc661d7a
Joao-ava <ajoao781238@gmail.com>
2025-09-30 16:13:54 -0300

    add: verify if graph is eulerian

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+from depth_first_search import DepthFirstSearch
+
+
+def is_eulerian(graph):
+    """
+    Verifica se o grafo possui uma trilha ou circuito euleriano.
+    :return: "circuito" ou dispara um erro em caso de não ser euleriano.
+    :raises ValueError: se o grafo não for conexo ou possuir mais de 0 vértices de grau ímpar.
+    """
+    odd = 0
+    start = None
+    for v in range(graph.V()):
+        if graph.degree(v) > 0 and start is None:
+            start = v
+
+        if graph.degree(v) % 2 != 0:
+            odd += 1
+
+    dfs = DepthFirstSearch(graph, start)
+    for v in range(graph.V()):
+        if graph.degree(v) > 0 and not dfs.marked(v):
+            raise ValueError("O grafo não é conexo.")
+
+    if odd == 0:
+        return "circuito"
+
+    raise ValueError(f"O grafo não possui é euleriano pois possui {odd} vertices de grau impar.")
